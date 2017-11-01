2a7c16ecc3233e657a4c5ca7d9272787377d4f6c
Angular 2 <Angular 2>
2017-11-02 00:21:17 +0530

    added country component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CountryComponent } from './country/country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/country/country.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Country } from '../model/Country';
+
+@Component({
+  selector: 'app-country',
+  templateUrl: './country.component.html',
+  styleUrls: ['./country.component.css']
+})
+export class CountryComponent implements OnInit {
+
+  countryList: Country[] = [];
+  countries;
+  selectedCountry: string;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  	this.countries = [
+  		{'id':'IN', 'name':'India'},
+  		{'id':'JP', 'name':'Japan'},
+  		{'id':'OM', 'name':'Oman'},
+  		{'id':'QA', 'name':'Qatar'},
+  		{'id':'SG', 'name':'Singapore'}
+  	];
+
+  	for(var i = 0 ; i < this.countries.length ; i++) {
+  		this.countryList.push(new Country(this.countries[i].id, this.countries[i].name));
+  	} 
+
+  }
+
+}
